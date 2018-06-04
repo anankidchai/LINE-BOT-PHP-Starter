db7c6dc5b3628634e981d9594aec03ac73d48002
Anan <anan.kidchai@gmail.com>
2018-06-04 10:11:03 +0700

    edit header verify

--- verify.php
+++ verify.php
@@ -1,13 +1,13 @@
 <?php
-// $access_token = 'qRujes6Bgimxxh89m5tVqWzQhY0O1pY6+EmnQuhEsCKffHIgcWrILViW1HObnVDDD5qBsnix/jwZoRjGY24WA6UnDzDa6aNWVQMwUh6Rn/CvUKVE1h5fVuOMapfYxW28HgHq1P4LJAHR/VACp1aJowdB04t89/1O/w1cDnyilFU=';
-$channelSecret = "61392793dd35be959d06344f64739cf6"; // Channel secret string
+$access_token = 'qRujes6Bgimxxh89m5tVqWzQhY0O1pY6+EmnQuhEsCKffHIgcWrILViW1HObnVDDD5qBsnix/jwZoRjGY24WA6UnDzDa6aNWVQMwUh6Rn/CvUKVE1h5fVuOMapfYxW28HgHq1P4LJAHR/VACp1aJowdB04t89/1O/w1cDnyilFU=';
+// $channelSecret = "61392793dd35be959d06344f64739cf6"; // Channel secret string
 $httpRequestBody = "Anan Kidchai"; // Request body string
 $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
 $signature = base64_encode($hash);
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $signature);
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
